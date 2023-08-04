c849eb26ac89be575b67034f5ec1f40d58de5d54
Efe <hbehar@syncad.com>
2023-08-04 15:26:52 +0200

    Create library project #1

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": ["standard-with-typescript", "prettier"],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": "tsconfig.eslint.json",
+        "tsconfigRootDir": __dirname + "/tsconfig",
+    },
+    "rules": {
+    }
+}

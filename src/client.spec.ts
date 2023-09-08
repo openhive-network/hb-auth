c559f1626ded9de12653ef450adf8008759d171d
Efe Behar <hbehar@syncad.com>
2023-09-08 13:42:54 +0200

    Investigate specific mock case later

--- src/client.spec.ts
+++ src/client.spec.ts
@@ -1,22 +1,25 @@
 import { Client } from "./client";
 
-jest.mock("./environment", () => ({
-  isSupportWebWorker: true,
-}));
+jest.mock("./environment", () => ({ isSupportWebWorker: true }));
 
 describe("Test Client", () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+    jest.clearAllMocks();
+  });
+
   test("creates an instance only by getClient method", () => {
     const client = Client.getClient();
     expect(client).toBeInstanceOf(Client);
   });
 
-  test("instance is the same", () => {
+  test("instance are the same", () => {
     const i1 = Client.getClient();
     const i2 = Client.getClient();
     expect(i1).toEqual(i2);
   });
 
-  test("handle if no web worker supported", () => {
-    Client.getClient();
-  });
+  test.todo("handle if no web worker supported");
+  // isSupportWebWorkerSpy = jest.spyOn(environment, 'isSupportWebWorker', 'get').mockReturnValue(false);
 });
+

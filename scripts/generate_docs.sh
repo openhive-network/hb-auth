f9d63f7f42bf4128e41a986dcc2a1590f9781950
Efe <hbehar@syncad.com>
2024-02-13 10:33:37 +0100

    Add proper pnpm version, replace npm with pnpm for corepack

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -11,10 +11,10 @@ PROJECT_DIR="${SCRIPTPATH}/../"
 # https://typedoc.org/guides/installation/#requirements
 pushd "${PROJECT_DIR}"
 mkdir -vp build
-npm exec -- typedoc --plugin typedoc-plugin-markdown --theme markdown --excludeInternal --hideBreadcrumbs --hideInPageTOC --tsconfig tsconfig.docs.json --out build/docs dist/hb-auth.d.ts
+pnpm exec -- typedoc --plugin typedoc-plugin-markdown --theme markdown --excludeInternal --hideBreadcrumbs --hideInPageTOC --tsconfig tsconfig.docs.json --out build/docs dist/hb-auth.d.ts
 mv build/docs/modules.md build/docs/_modules.md
 rm build/docs/README.md
-npm exec -- concat-md --decrease-title-levels build/docs > api.md
+pnpm exec -- concat-md --decrease-title-levels build/docs > api.md
 popd
 
 UNSTAGED_FILES=($(git diff --name-only))

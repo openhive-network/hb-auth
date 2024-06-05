7e8f44f753030d5d29aee839eec83baf362606eb
Efe <hbehar@syncad.com>
2024-06-05 10:59:06 +0200

    Fix keytype prop in example app

--- example/app.js
+++ example/app.js
@@ -22,7 +22,7 @@ client.initialize().then(async (authClient) => {
         statusEl.style.color = "grey";
       } else {
         if (auth.authorized) {
-          statusEl.innerHTML = `Authorized with username: <b>${auth.username}</b> and keyType: ${auth.keyType}`;
+          statusEl.innerHTML = `Authorized with username: <b>${auth.username}</b> and keyType: ${auth.loggedInKeyType}`;
           statusEl.style.color = "green";
         } else {
           statusEl.innerHTML = `User: <b>${auth.username}</b> requires authorization`;
